1d721adbe3f579464537cabd23384c794b2d5f3d
Santiago-School <a.23308051140421@cbtis128.edu.mx>
2024-09-26 16:22:35 -0600

    Add files via upload

---
+++ Carrasco_2_Santiago_0421_W_5.py
@@ -0,0 +1,11 @@
+# leer dos valores
+valor1 = float(input("introduce el primer valor: "))
+valor2 = float(input("introduce el segundo valor: "))
+
+# determinar el menor
+menor = min(valor1, valor2)
+print("el menor es:", menor)
+
+# sumar los dos valores
+suma = valor1 + valor2
+print("la suma es:", suma)
